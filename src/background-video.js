39bddbd62f767953fed1943d70d5b6606f48de98
Elian05 <el.barram@duocuc.cl>
2025-05-04 18:41:52 -0400

    changeviewv2

---
+++ src/background-video.js
@@ -0,0 +1,25 @@
+const video = document.getElementById("background-video");
+let mouseMoveTimeout;
+
+video.addEventListener("loadeddata", () => {
+  video.pause();
+});
+
+// Reproduce el video cuando el mouse se mueve
+document.body.addEventListener("mousemove", () => {
+  if (video.paused) {
+    video.play();
+  }
+
+  // Reinicia el temporizador cada vez que el mouse se mueve
+  clearTimeout(mouseMoveTimeout);
+  mouseMoveTimeout = setTimeout(() => {
+    video.pause(); // Pausa el video si el mouse está quieto
+  }, 2000); // Pausa después de 2 segundos sin movimiento
+});
+
+// Pausa el video cuando el mouse sale del cuerpo
+document.body.addEventListener("mouseleave", () => {
+  video.pause();
+  clearTimeout(mouseMoveTimeout);
+});
